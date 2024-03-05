8d1d1b833bbba1a8b986fb6c1e68aab6a412ff9c
Umar <umarnuridinov@umars-MacBook-Air.local>
2024-03-05 16:39:52 +0600

    resolve conflicts

--- src/components/listAddmission/index.jsx
+++ src/components/listAddmission/index.jsx
@@ -4,27 +4,27 @@ import "./style.scss";
 import product from "../../assets/phone.png";
 
 const data = [
-    {
-        img: { product },
-        price: 100415,
-        title: "  Смартфон Apple iPhone 14 Pro Max 256GB EU",
-        description:
-            "iPhone 14 Pro MaxВолшебный новый способ взаимодействия с iPhone. .....",
-    },
+  {
+    img: { product },
+    price: 100415,
+    title: "  Смартфон Apple iPhone 14 Pro Max 256GB EU",
+    description:
+      "iPhone 14 Pro MaxВолшебный новый способ взаимодействия с iPhone. .....",
+  },
 ];
 
 function List() {
-    return (
-        <div className="container">
-                <h1 className="list_title">Новое поступление</h1>
-            <div className="list">
-                <ItemAddmission data={data} />
-                <ItemAddmission />
-                <ItemAddmission />
-                <ItemAddmission />
-            </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1 className="list_title">Новое поступление</h1>
+      <div className="list">
+        <ItemAddmission data={data} />
+        <ItemAddmission />
+        <ItemAddmission />
+        <ItemAddmission />
+      </div>
+    </div>
+  );
 }
 
 export default List;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/header/header";
 import Cards from "../components/cards";
 import Footer from "../components/footer/footer";
 import Brands from "../components/brands/brand";
+import List from "../components/listAddmission";
 
 function Home() {
   return (
